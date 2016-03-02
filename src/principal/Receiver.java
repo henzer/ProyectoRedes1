49ee173ef1ed37bf1b4c33c0134a91042b17eff0
Henzer <Henzer@HENZER-PC>
2016-03-02 16:31:05 -0600

    Se agrego el socket de conexion con el servidor.

--- src/principal/Receiver.java
+++ src/principal/Receiver.java
@@ -1,22 +1,55 @@
 package principal;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.channels.SocketChannel;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Receiver implements Runnable{
+public class Receiver{
     private String IP;
     private int PORT;
+    private static Socket socket;
+    private static ObjectInputStream in;
+    private static ObjectOutputStream out;
 
+    //Constructor de la clase, recibe la IP y el puerto de destino
     public Receiver(String IP, int PORT) {
         this.IP = IP;
         this.PORT = PORT;
+        try {
+            socket = new Socket(IP, PORT);
+            in = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-    @Override
+    
+    //Metodo para enviar información al servidor.
+    public static void send(String type, Object data) throws IOException{
+        if(type.equals("String")){
+            String s = (String)data;
+            out.writeBytes(s);
+        }else if(type.equals("Player")){
+            Player p = (Player)data;
+            out.writeObject(p);
+        }
+    }
+    
+    public static String getData() throws IOException{
+        return in.readUTF();
+    }
+    
+    public static Player getDataPlayer() throws IOException, ClassNotFoundException{
+        Player player = (Player)in.readObject();
+        return player;
+    }
+    
+/*
     public void run() {
         try {
             SocketChannel sChannel = SocketChannel.open();
@@ -43,5 +76,5 @@ public class Receiver implements Runnable{
             ex.printStackTrace();
         }
     }
-
+*/
 }
